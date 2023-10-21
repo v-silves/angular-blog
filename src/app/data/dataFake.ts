3f358a37674a2fe8187dc2c4a43002cb6730c4be
Victória Silvestre <143358954+v-silves@users.noreply.github.com>
2023-10-21 00:44:03 -0300

    Update dataFake.ts
    Alterando conteúdos da data-fakes.

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,14 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "title": "BARBIE APARTA BRIGA ENTRE GIGANTES",
+    "description": "Nossa heroína flagrada em ação, impedindo que dois gigantes dos streamings saíssem no tapa. Boatos de que eles esperavam por esse dia há muito tempo, porém agora retornam para casa com suas expextativas frustradas). Barbie conseguiu manter a paz dessa vez... O que será do amanhã?",
+    "photoCover":"https://s2-techtudo.glbimg.com/rjoe-lrtwPpQbHM-X8bRsOpyECk=/0x0:1600x900/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/g/r/gwA3D3Ru6yWrL7aZILbQ/wirestory-88552e6e78b9618df9719e77fe0d494c-16x9-1600-copiar.jpg"
   },
   {
     "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "title": "Vigiando Barbilândia: ainda é crime não se vestir de rosa",
+    "description": "Barbie revela método para vigiar nossa cidade: ela procura diariamente barbilandianos que não estejam vestindo, pelo menos, uma peça de roupa ou acessório cor de rosa. Atentem-se, barbilandianos, não é somente às quartas que vestimos rosa!",
+    "photoCover":"https://classic.exame.com/wp-content/uploads/2022/12/whatsapp-image-2022-12-16-at-15.13.31.jpeg?quality=70&strip=info&w=984"
   }
 ]
